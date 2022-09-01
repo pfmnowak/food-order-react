141b0a992cddd5181fbed2e032433b628fa00b55
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-09-01 10:55:25 +0200

    Add Meals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meal/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
